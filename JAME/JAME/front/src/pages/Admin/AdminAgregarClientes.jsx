d2e9c053bbe9f85c86f68961ecc69b0507e0bf13
leetosky27 <leetosky27@gmail.com>
2025-03-19 18:37:21 -0500

    enrutamiento de clientes y mascotas

--- JAME/JAME/front/src/pages/Admin/AdminAgregarClientes.jsx
+++ JAME/JAME/front/src/pages/Admin/AdminAgregarClientes.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Form, Button, Container, Card } from "react-bootstrap";
 import { Link } from 'react-router-dom';
 import Footer from '../../components/Footer';
-import Navbar from '../../components/Navbar';
 
     const AgregarCliente = () => {
         const [cliente, setCliente] = useState({
@@ -40,19 +39,19 @@ import Navbar from '../../components/Navbar';
             <div className="d-flex flex-grow-1">
                 <div className="bg-dark text-white p-0 d-flex flex-column" style={{ width: '200px' }}>
                     <div className="list-group list-group-flush">
-                        <a href="#" className="list-group-item list-group-item-action bg-success text-white py-3">
+                        <a href="/administrador" className="list-group-item list-group-item-action bg-dark text-white py-3">
                             <i className="bi bi-house me-2"></i> Inicio
                         </a>
-                        <a href="#" className="list-group-item list-group-item-action bg-dark text-white py-3">
+                        <a href="/ventas" className="list-group-item list-group-item-action bg-dark text-white py-3">
                             <i className="bi bi-cart me-2"></i> Ventas
                         </a>
-                        <a href="#" className="list-group-item list-group-item-action bg-dark text-white py-3">
+                        <a href="/agendamientos" className="list-group-item list-group-item-action bg-dark text-white py-3">
                             <i className="bi bi-calendar2 me-2"></i> Agendamientos
                         </a>
-                        <a href="#" className="list-group-item list-group-item-action bg-dark text-white py-3">
+                        <a href="/pedidos" className="list-group-item list-group-item-action bg-dark text-white py-3">
                             <i className="bi bi-box me-2"></i> Pedidos
                         </a>
-                        <a href="#" className="list-group-item list-group-item-action bg-dark text-white py-3">
+                        <a href="/reportes" className="list-group-item list-group-item-action bg-dark text-white py-3">
                             <i className="bi bi-bar-chart-line"></i> Reportes
                         </a>
                         <a href="#" className="list-group-item list-group-item-action bg-dark text-white py-3 mt-auto">
