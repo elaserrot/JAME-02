034bed868cba749132928935b71c3ad9ba14f162
elaserrot <125906393+Jjuparela2007@users.noreply.github.com>
2025-03-20 14:45:19 -0500

    actuaalizacion cuadro de observaciones mascotas

--- JAME/JAME/front/src/pages/Admin/AdminMascotas.jsx
+++ JAME/JAME/front/src/pages/Admin/AdminMascotas.jsx
@@ -15,12 +15,12 @@ export default function AdminMascotas() {
         // Simulación de carga de datos
         const data = [
 
-            { id: 1, nombre: 'keyla',imagen: '/src/img/keyla.jpg', fecha: '16/10/2022',especie: 'Perro', raza: 'Bulldog french', genero: 'hembra', edad: 3, dueño: 'Alisson' },
-            { id: 2, nombre: 'Napoleon',imagen: '/src/img/napoleon.jpg', fecha: '28/06/2023',especie: 'Perro', raza: 'Bulldog french', genero: 'macho', edad: 2, dueño: 'Juan' },
-            { id: 3, nombre: 'Rocky',imagen: '/src/img/rocky.png',fecha: '10/02/2024', especie: 'Gato',  raza: 'leopardo',       genero: 'macho', edad: 1, dueño: 'Gilma' },
-            { id: 4, nombre: 'Toby', imagen: '/src/img/toby.png',fecha: '6/08/2021', especie: 'Perro', raza: 'rotwailler',       genero: 'macho', edad: 4, dueño: 'Martin' },
-            { id: 5, nombre: 'Jack',imagen: '/src/img/jack.png', fecha: '4/12/2022', especie: 'Gato',  raza: 'tigrillo',       genero: 'macho', edad: 3, dueño: 'Diego' },
-            { id: 6, nombre: 'Max', imagen: '/src/img/max.jpg', fecha: '26/07/2023 ', especie: 'Gato',  raza: 'siames',         genero: 'macho', edad: 2, dueño: 'Carlos' }
+            { id: 1, nombre: 'keyla',imagen: '/src/img/keyla.jpg', fecha: '16/10/2022',especie: 'Perro', raza: 'Bulldog french', genero: 'hembra', edad: 3, estado: 'Activo', dueño: 'Alisson Torres',documento: '1025532382', telefono: '3214138943', email: 'alidatorres0623@gmail.com', direccion: 'calle 15a #37-22', observaciones: 'Gonzo le falta un bracito jaja' },
+            { id: 2, nombre: 'Napoleon',imagen: '/src/img/napoleon.jpg', fecha: '28/06/2023',especie: 'Perro', raza: 'Bulldog french', genero: 'macho', edad: 2, estado: 'Activo', dueño: 'Juan José',documento: '1020738735', telefono: '3138332309', email: 'uparelajuan2@gmail.com', direccion: 'calle 16a #80-64', observaciones:'Peligroso, le dicen asesino' },
+            { id: 3, nombre: 'Rocky',imagen: '/src/img/rocky.png',fecha: '10/02/2024', especie: 'Gato',  raza: 'leopardo',       genero: 'macho', edad: 1, estado: 'Activo', dueño: 'Gilma',documento: '1563289612', telefono: '3130456982', email: 'gilma@gmail.com', direccion: 'carrera 10a #37-22', observaciones: ''},
+            { id: 4, nombre: 'Toby', imagen: '/src/img/toby.png',fecha: '6/08/2021', especie: 'Perro', raza: 'rotwailler',       genero: 'macho', edad: 4, estado: 'Activo', dueño: 'Martin',documento: '1896423584', telefono: '3138956722', email: 'martin@gmail.com', direccion: 'transversal 31a #37-22', observaciones: '' },
+            { id: 5, nombre: 'Jack',imagen: '/src/img/jack.png', fecha: '4/12/2022', especie: 'Gato',  raza: 'tigrillo',       genero: 'macho', edad: 3, estado: 'Activo', dueño: 'Diego',documento: '1089532475', telefono: ' 3172304896', email: 'diego@gmail.com', direccion: 'calle 22a #37-22', observaciones: '' },
+            { id: 6, nombre: 'Max', imagen: '/src/img/max.jpg', fecha: '26/07/2023 ', especie: 'Gato',  raza: 'siames',         genero: 'macho', edad: 2, estado: 'Activo', dueño: 'Carlos',documento: '1524861357', telefono: '3154892176', email: 'carlos@gmail.com', direccion: 'diagonal 23a #37-22', observaciones: '' }
               
 
         ];
@@ -105,7 +105,14 @@ export default function AdminMascotas() {
                             <p><strong>Raza:</strong> {mascotaEncontrada.raza}</p>
                             <p><strong>Genero:</strong> {mascotaEncontrada.genero}</p>
                             <p><strong>Edad:</strong> {mascotaEncontrada.edad} años</p>
+                            <p><strong>Estado:</strong> {mascotaEncontrada.estado}</p>
                             <p><strong>Dueño:</strong> {mascotaEncontrada.dueño}</p>
+                            <p><strong>Documento:</strong> {mascotaEncontrada.documento}</p>
+                            <p><strong>Telefono:</strong> {mascotaEncontrada.telefono}</p>
+                            <p><strong>Email:</strong> {mascotaEncontrada.email}</p>
+                            <p><strong>Direccion :</strong> {mascotaEncontrada.direccion}</p>
+                            <p><strong>Observaciones :</strong> {mascotaEncontrada.observaciones}</p>
+
                         </div>
                     )}
                     {/* Lista de mascotas */}
@@ -132,28 +139,51 @@ export default function AdminMascotas() {
                     </div>
 
                     {/* Modal para mostrar información de la mascota */}
-                    <Modal show={mascotaSeleccionada !== null} onHide={() => setMascotaSeleccionada(null)}size="lg">
+                    <Modal show={mascotaSeleccionada !== null} onHide={() => setMascotaSeleccionada(null)}size="xl">
                         <Modal.Header closeButton className="bg-primary text-white">
                             <Modal.Title><h1>{mascotaSeleccionada?.nombre}</h1></Modal.Title>
                         </Modal.Header>
                         <Modal.Body>
-                            <div className="mb-5 d-flex gap-5">
+                        <div className="container">
+                        <div className="row d-flex align-items-start">
+                        <div className="col-md-4">
                             <img 
                             src={mascotaSeleccionada?.imagen || "/src/img/dog_placeholder.png"} 
                             alt={mascotaSeleccionada?.nombre} 
                             className="img-fluid rounded"
-                            style={{ height: "400px", objectFit: "cover"}}/>
-                            
-                                <div className="col-md-12">
+                            style={{ height: "300px", objectFit: "cover"}}/>
+                            </div>
+                    
+                                <div className="col-md-4">
                                     <p><b><strong>Fecha de nacimiento:</strong></b> {mascotaSeleccionada?.fecha}</p>
                                     <p><b><strong>Especie:</strong></b> {mascotaSeleccionada?.especie}</p>
                                     <p><b><strong>Raza:</strong></b> {mascotaSeleccionada?.raza}</p>
                                     <p><b><strong>Género:</strong></b> {mascotaSeleccionada?.genero}</p>
                                     <p><b><strong>Edad:</strong></b> {mascotaSeleccionada?.edad} años</p>
+                                    <p><b><strong>Estado:</strong></b> {mascotaSeleccionada?.estado}</p>
+                                </div>
+                                
+                               
+                                <div className="col-md-4">
                                     <p><b><strong>Dueño:</strong></b> {mascotaSeleccionada?.dueño}</p>
+                                    <p><b><strong>Documento:</strong></b> {mascotaSeleccionada?.documento}</p>
+                                    <p><b><strong>Telefono:</strong></b> {mascotaSeleccionada?.telefono}</p>
+                                    <p><b><strong>Email:</strong></b> {mascotaSeleccionada?.email}</p>
+                                    <p><b><strong>Direccion:</strong></b> {mascotaSeleccionada?.direccion}</p>
+                                    
                                     
                                 </div>
                             </div>
+                            </div>
+                            {/* Sección de Observaciones */}
+                            <div className="row mt-4">
+                                <div className="col-12">
+                                <div className="border p-3 rounded">
+                                    <h5 className="fw-bold">Observaciones</h5>
+                                    <p>{mascotaSeleccionada?.observaciones || "No hay observaciones registradas."}</p>
+                                </div>
+                            </div>
+                        </div>
                         </Modal.Body>
                         <Modal.Footer>
                             <Button variant="secondary" onClick={() => setMascotaSeleccionada(null)}>Cerrar</Button>
