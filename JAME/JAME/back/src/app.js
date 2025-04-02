48c5a632b314088ef083a9b922c2dbac6c1a7404
leetosky27 <leetosky27@gmail.com>
2025-04-02 10:01:46 -0500

    configuracion de los cors por politica de funcionamiento

--- JAME/JAME/back/src/app.js
+++ JAME/JAME/back/src/app.js
@@ -1,10 +1,21 @@
 const express = require('express')
+const cors = require('cors')
 const dotenv = require('dotenv')
 const usersRoutes = require('./routes/usersRoutes')
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const corsOptions = {
+    origin: '*',  
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',  
+    allowedHeaders: 'Content-Type,Authorization',  
+    credentials: true, 
+};
+
+app.use(cors(corsOptions));  
+
 
 // Middleware para analizar el cuerpo de las solicitudes
 app.use(express.json());
